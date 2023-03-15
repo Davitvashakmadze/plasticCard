6a4b103d096d1a1bcd9c116b89130ddaea51b678
Datuchaaa <vashaymadzedavit@gmail.com>
2023-03-15 15:45:55 +0400

    init: initializing project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/Home/Home';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../Home/Home.scss'
+
+const Home = () => {
+  return (
+    <div>
+      <section className="home-section">
+        <div className="left-section"></div>
+        <div className="right-section">
+          
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
+
+
