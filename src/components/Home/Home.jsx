2690040a23317209b0c380ecde176415835c58ba
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-03-31 22:28:12 +0400

    feat: add thanks page with buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Home/Home.scss";
 import { useState } from "react";
-// import Forms from "./Forms";
+// import Thenks from "./Thenks";
 import { Input, Label, Button } from "reactstrap";
 
 import lines from "../../images/lines.png";
@@ -12,17 +12,20 @@ const Home = () => {
   const [cardMonth, setCardMonth] = useState("00");
   const [cardYear, setCardYear] = useState("00");
   const [cardCvc, setCardCvs] = useState("000");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   // const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (event) => {
-    setCardOwnerValue(event.target.value);
+    setCardOwnerValue(event.target.value.toUpperCase());
   };
 
   const handleCardNumberChange = (event) => {
-    let value = event.target.value;
-    // value = value.replace(/\s/g, ""); // remove existing spaces
-    value = value.replace(/(\d{4}(?=\d))/g, "$1 "); // add space after every 4 digits
+    const value = event.target.value
+      .replace(/[^\d]/g, "") // remove all non-digit characters
+      .replace(/(.{4})/g, "$1 ") // add a space after every 4 digits
+      .trim(); // remove any trailing spaces
+
     setCardNumber(value);
   };
 
@@ -35,6 +38,13 @@ const Home = () => {
   };
   const handleCardCvcChange = (event) => {
     setCardCvs(event.target.value);
+  };
+  const hanleConfirm = () => {
+    setIsSubmitted(true);
+  };
+
+  const handleClickRefresh = () => {
+    window.location.reload();
   };
 
   return (
@@ -69,77 +79,90 @@ const Home = () => {
         <div className="right-section">
           <div className="right-section">
             <div className="input-wrapper">
-              <form autocomplete="off">
-                <div className="card-name">
-                  <Label for="card-name">Cardholder Name</Label>
-                  <Input
-                    onChange={handleInputChange}
-                    type="text"
-                    id="card-name"
-                    name="card-name"
-                    placeholder="e.g. Jane Appleseed"
-                  />
-                </div>
-                <div className="card-number">
-                  <Label for="card-number">Card Number</Label>
-                  <Input
-                    type="text"
-                    id="card-number"
-                    name="card-number"
-                    maxLength={19}
-                    placeholder="e.g. 1234 5678 9123 0000"
-                    onChange={handleCardNumberChange}
-                    value={cardNumber}
-                    required
-                  />
-                </div>
-                <div className="card-data">
-                  <div className="dates-input">
-                    <div className="date-label">
-                      <Label for="card-date">Exp. Date</Label>
-                      <Input
-                        onChange={handleCardMonthChange}
-                        className="date-input"
-                        maxLength={2}
-                        type="text"
-                        id="card-month"
-                        name="month"
-                        placeholder="MM"
-                        required
-                      />
-                    </div>
-                    <div className="date-label">
-                      <Label for="card-date">(MM/YY)</Label>
-                      <Input
-                        onChange={handleCardYearChange}
-                        className="date-input"
-                        maxLength={2}
-                        type="text"
-                        id="card-year"
-                        name="year"
-                        placeholder="YY"
-                        required
-                      />
-                    </div>
+              {!isSubmitted ? (
+                <form autocomplete="off">
+                  <div className="card-name">
+                    <Label for="card-name">Cardholder Name</Label>
+                    <Input
+                      onChange={handleInputChange}
+                      type="text"
+                      id="card-name"
+                      name="card-name"
+                      placeholder="e.g. Jane Appleseed"
+                    />
+                  </div>
+                  <div className="card-number">
+                    <Label for="card-number">Card Number</Label>
+                    <Input
+                      type="text"
+                      id="card-number"
+                      name="card-number"
+                      maxLength={19}
+                      placeholder="e.g. 1234 5678 9123 0000"
+                      onChange={handleCardNumberChange}
+                      value={cardNumber}
+                      required
+                    />
+                  </div>
+                  <div className="card-data">
+                    <div className="dates-input">
+                      <div className="date-label">
+                        <Label for="card-date">Exp. Date</Label>
+                        <Input
+                          onChange={handleCardMonthChange}
+                          className="date-input"
+                          maxLength={2}
+                          type="text"
+                          id="card-month"
+                          name="month"
+                          placeholder="MM"
+                          required
+                        />
+                      </div>
+                      <div className="date-label">
+                        <Label for="card-date">(MM/YY)</Label>
+                        <Input
+                          onChange={handleCardYearChange}
+                          className="date-input"
+                          maxLength={2}
+                          type="text"
+                          id="card-year"
+                          name="year"
+                          placeholder="YY"
+                          required
+                        />
+                      </div>
 
-                    <div className="cvc-label">
-                      <Label htmlFor="CVC">CVC</Label>
-                      <Input
-                        onChange={handleCardCvcChange}
-                        placeholder="e.g. 123"
-                        className="cvc-input"
-                        maxLength={3}
-                        type="text"
-                        id="cvc"
-                        name="cvc"
-                      />
+                      <div className="cvc-label">
+                        <Label htmlFor="CVC">CVC</Label>
+                        <Input
+                          onChange={handleCardCvcChange}
+                          placeholder="e.g. 123"
+                          className="cvc-input"
+                          maxLength={3}
+                          type="text"
+                          id="cvc"
+                          name="cvc"
+                        />
+                      </div>
                     </div>
                   </div>
+                  <div className="sub-btn">
+                    <Button onClick={hanleConfirm}>Confirm</Button>
+                  </div>
+                </form>
+              ) : (
+                <div className="thanks-section">
+                  <div className="done">
+                    <i class="fas fa-check"></i>
+                  </div>
+                  <h2>THENK YOU</h2>
+                  <p>We’ve added your card details</p>
+                  <div className="cnt-btn">
+                    <Button onClick={handleClickRefresh}>Continue</Button>
+                  </div>
                 </div>
-              </form>
-              <div className="sub-btn">
-                <Button>Confirm</Button>
-              </div>
+              )}
             </div>
           </div>
         </div>
